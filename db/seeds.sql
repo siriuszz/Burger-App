1f272090861d5de99486bc1f40d3b338e40d9932
Star Lee <zabra15@yahoo.com>
2017-11-13 14:39:13 -0500

    server.js plus db files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bleu Cheese Burger");
---
+++ server.js
@@ -0,0 +1,3 @@
+var express = require("express");
+var override = require("method-override");
+var parser = require("body-parser");
