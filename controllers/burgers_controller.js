5bbad6148e6519182d0b1640233af7829ca911c5
Star Lee <zabra15@yahoo.com>
2017-11-17 18:43:40 -0500

    removed PUT and DELETE routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,33 +24,6 @@ router.post("/api/burgers", function (req, res) {
     });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
 
 //Export for server.js
 module.exports = router;
